c8154a877c87e1905a35b3a05bb7de985e046e0f
MiriamAranha <miriam.hortencio@escola.pr.gov.br>
2025-06-10 08:36:32 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
-    'Conhecimentos gerais',
+    'Conhecimentos gerais sobre o brawl stars',
     'Qual é o nome do primeiro Brawler lendário introduzido no Brawl Stars?',
     'Spike'
 )
 
 criaCartao(
-    'Conhecimentos gerais',
+    'Conhecimentos gerais sobre o brawl stars',
     'Qual modo de jogo envolve coletar 10 gemas e mantê-las até o final?',
     'Pique-gema'
 )
 
 criaCartao(
-    'Conhecimentos gerais',
+    'Conhecimentos gerais sobre o brawl stars',
     'Qual Brawler tem a habilidade estelar chamada "Curacora"?',
     'Poco'
 )
 
 criaCartao(
-    'Conhecimentos gerais',
+    'Conhecimentos gerais sobre o brawl stars',
     'Quantos pontos de troféu são necessários para desbloquear o Passe de Batalha gratuito?',
     '3000 troféus'
 )
